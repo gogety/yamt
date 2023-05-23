96100c3f2ff419a11edbbe8fd17bc15e3c7ab996
Saman Samy Sarem <sarem.saman@yahoo.fr>
2023-05-23 02:48:28 -0400

    added default channels; changed offline/online detection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,86 +1,68 @@
 <script lang="ts">
-	import { tick } from 'svelte';
-  import Grid from '../grid/grid.svelte'
+	import { onMount, tick } from 'svelte';
+	import Grid from '../grid/grid.svelte';
+	import { myChannels } from './myChannels';
 
-  let pool:string[] = []
-  const MAXPOOL = 10;
-  let channelsToAdd:string[]=[];
-
-  let handleTwitchReady= (event:CustomEvent)=>
-  {
-    const channel = event.detail.data;
-    if (pool.includes(channel)){
-      // alert(`freeing ${channel}`) 
-      pool = pool.filter(e=>e!=channel);
-    }
-  }
-
-  let addChannel=()=>{
-    let currentValue:string = document.getElementById("channel_name").value;
-
-    try{
-      currentValue= JSON.parse(currentValue);
-    }
-    catch{ }
-
-    channelsToAdd = Array.isArray(currentValue)?[...currentValue]:[currentValue];
-    // typeof(currentValue) =="Array"{
-    //   channelsToAdd = currentValue;
-    // }
-    // else{
-    //   channelsToAdd = [currentValue];
-    // }
-    debugger;
-    
-    // easiest but also can be too much in one shot
-    // onlineChannels=[...new Set([...onlineChannels, ...channelsToAdd])];
-    
-    // lazy buffering
-    // const addChannelSlowly = () => {
-    //   if (channelsToAdd.length > 0){
-    //     if(pool.length < MAXPOOL){
-    //       const channel = channelsToAdd.shift();
-    //       channelsToAdd = channelsToAdd;
-    //       channel && !channels.includes(channel) && channels.push(channel) && (channels = channels) && (pool.push(channel))
-    //     }
-    //     setTimeout(addChannelSlowly,100)
-    //   }
-    // }
-    // addChannelSlowly();
-
-    // best buffering would be to have a "pool" and wait for the events to free more space
-    
-    // if (!channels.includes(currentValue)){
-      // channels.push(currentValue);
-      // }
-    }
-
-  // Sort of recursion with tick. Updating channelsToAdd and/or pool triggers the function again
-  $: tick().then(()=>{
-    if(channelsToAdd.length > 0 && pool.length < MAXPOOL){
-      const channel = channelsToAdd.shift();
-      channelsToAdd = channelsToAdd;
-      channel && !channels.includes(channel) && channels.push(channel) && (channels = channels) && (pool.push(channel)) && (pool=pool)
-    }
-  });
-
-  // import Twitch from '../twitch/TwitchChannelEmbed.svelte';
-  export let channels: Array<string> = ['davegrecoart','dzikawa','monstercat'];
-  // export let offlineChannels: Array<string>=[]; 
+	let pool: string[] = [];
+	const MAXPOOL = 30;
+	let channelsToAdd: string[] = [];
+	export let channels: Array<string> = [];
   let displayOfflineChannels = false;
+  let input:string;
+
+  onMount(async()=>{
+
+  })
+
+
+	let handleTwitchReady = (event: CustomEvent) => {
+		const channel = event.detail.data;
+		if (pool.includes(channel)) {
+			// alert(`freeing ${channel}`)
+			pool = pool.filter((e) => e != channel);
+		}
+	};
+
+	let addChannels = () => {
+		// let currentValue: string = document.getElementById('channel_name').value;
+    let currentValue = input;
+
+		try {
+			currentValue = JSON.parse(currentValue);
+		} catch {}
+
+		channelsToAdd = Array.isArray(currentValue) ? [...currentValue] : [currentValue];
+
+		debugger;
+
+		// easiest to push all channels but also can be too much in one shot. Comment if enabling reactive buffering below
+		channels = [...new Set([...channels, ...channelsToAdd])];
+	};
+
+	// Reactive buffering, not needed when there are not that many online channels
+	// Sort of recursion with tick. Updating channelsToAdd and/or pool triggers the function again
+	// $: tick().then(()=>{
+	//   if(channelsToAdd.length > 0 && pool.length < MAXPOOL){
+	//     const channel = channelsToAdd.shift();
+	//     channelsToAdd = channelsToAdd;
+	//     channel && !channels.includes(channel) && channels.push(channel) && (channels = channels) && (pool.push(channel)) && (pool=pool)
+	//   }
+	// });
+
+	// export let offlineChannels: Array<string>=[];
+
+    input = myChannels;
+
 </script>
 
-<input type="text" id="channel_name"/>
-<button on:click="{()=>addChannel()}" >Add</button>
-{#if pool.length > 0 }
-<label style="color: white;">Loading {pool.length} [{pool.join(',')}], {channelsToAdd.length} channels to go </label>
+<input bind:value={input} placeholder="Enter channel name or JSON array of channels" type="text" id="channel_name" />
+<button on:click={() => addChannels()}>Add</button>
+{#if pool.length > 0}
+	<label style="color: white;">Loading {pool.length} [{pool.join(',')}], {channelsToAdd.length} channels to go</label>
 {/if}
-<input type="checkbox" bind:checked={displayOfflineChannels}/>
-  <label style="color: white;">Display offline channels</label>
-<Grid on:twitchready={handleTwitchReady}
-  displayOfflineChannels={displayOfflineChannels} 
-  channels={channels}></Grid>
+<input type="checkbox" bind:checked={displayOfflineChannels} />
+<label style="color: white;">Display offline channels</label>
+<Grid on:twitchready={handleTwitchReady} {displayOfflineChannels} {channels} />
 
 <style>
-
 </style>
--- src/twitch/twitch.svelte
+++ src/twitch/twitch.svelte
@@ -1,57 +1,88 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { createEventDispatcher } from 'svelte';
+	import { isChannelLive } from './TwitchUtils';
   const dispatch = createEventDispatcher();
   let player:Twitch.Player;
+  let isLive:boolean=false;
   export let channelName:string= "monstercat"
   export let showChat:boolean = false
   export let onlineHandler = ()=>{};
   export let offlineHandler = ()=>{};
 
+  const updateIsLive = async ()=>{
+      isLive = await isChannelLive(channelName);
+      if (!isLive){
+        offlineHandler();
+        dispatch('twitchready', {data: channelName});
+        setTimeout(()=>updateIsLive(),60000);
+      }
+      return isLive;
+    }
+
   onMount(async () => {
-    player = new Twitch.Embed(`twitch-embed-${channelName}`, {
-      width: "100%",
-      height: "100%",
-      channel: channelName,
-      muted: true,
-      // Only needed if this page is going to be embedded on other websites
-      parent: ["localhost"],
-      layout: showChat?"video-with-chat":"video"
-    });
-    player.addEventListener(Twitch.Embed.READY,function(){
-      if (showChat){
-        player.setQuality(player.getQuality()[1])
-        document.documentElement.requestFullscreen();
-      }
-      else{
-        // lazy, for now statically get the second highest 
-        player.setQuality('480p');
-      }
-      // alert (`sending ${channelName}`)
-      dispatch('twitchready', {data: channelName});
-    })
-    player.addEventListener(Twitch.Embed.VIDEO_READY, function(){
-      window.player=player;
-    })
-    player.addEventListener(Twitch.Player.OFFLINE, function(){
-      // alert("channel is offline !");
-      offlineHandler();
-    })
-    player.addEventListener(Twitch.Player.ONLINE, function(){
-      onlineHandler();
-    })
+    updateIsLive()
   })
   $: {
     if (player){
       player.setChannel(channelName);
     } 
   }
+  $: {
+    if(isLive){
+      player = new Twitch.Embed(`twitch-embed-${channelName}`, {
+        width: "100%",
+        height: "100%",
+        channel: channelName,
+        muted: true,
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["localhost"],
+        layout: showChat?"video-with-chat":"video"
+      });
+      player.addEventListener(Twitch.Embed.READY,function(){
+        if (showChat){
+          player.setQuality(player.getQuality()[1])
+          document.documentElement.requestFullscreen();
+        }
+        else{
+          // lazy, for now statically get the second highest 
+          player.setQuality('480p');
+        }
+        dispatch('twitchready', {data: channelName});
+      })
+      player.addEventListener(Twitch.Embed.VIDEO_READY, function(){
+        window.player=player;
+      })
+      player.addEventListener(Twitch.Player.OFFLINE, function(){
+        // alert("channel is offline !");
+        updateIsLive()
+      })
+      player.addEventListener(Twitch.Player.ONLINE, function(){
+        onlineHandler();
+      })
+    }
+   };
 </script>
-<div class="twitch-embed" id="twitch-embed-{channelName}"></div>
+
+<div class="twitch-embed" id="twitch-embed-{channelName}">
+  {#await updateIsLive}
+  <div> Loading... </div>
+  {:then}
+    {#if !isLive}
+    <div class="offline">{channelName} is offline!</div>
+    {/if}
+  {/await}
+</div>
+
 <!-- Add a placeholder for the Twitch embed -->
 <!-- <div class="twitch-embed" id="twitch-embed-{channelName}"></div> -->
 <style>
    .twitch-embed {
 		aspect-ratio: 16/9;
 	}
+
+  .offline{
+    vertical-align: middle;
+    text-align: center;
+  }
 </style>
